3883cbc37455d09c87030599dc630ba4669c1c84
evllz <erickvillegsaecheverria@gmail.com>
2020-09-26 01:18:21 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import './components/Character'
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
+  const [list,setList] = useState([]);
+  const [initialRender,setInitalRender] = useState(false)
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  useEffect(() => {
+    if(initialRender === false){
+      axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+      .then(response => {setList(response.data.results);setInitalRender(true)});
+    }
+  },[initialRender]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
-  return (
+   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Pokemon</h1>
+      {initialRender ? <Character list={list}/>: <p>Loadging....</p>}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,99 @@
 // Write your Character component here
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const Search = styled.input`
+font-size: 15px;
+text-align: left;
+height: 20px;
+with: 30px;
+`;
+
+const Button = styled.button`
+font-size: 15px;
+background: white;
+color: black;
+height: 25px;
+with: 30px;
+`;
+
+const Image = styled.img`
+width: 200px;
+height: 200px
+`;
+const Container = styled.div`
+font-weight: bold;
+background: #FFCB05;
+border: 5px solid #3B5CA8;
+width: 200px;
+height: 400px
+align-content: center;
+margin: 2px
+padding: 2px;
+display: flex;
+flex-direction: row; 
+justify-content: center;
+margin-left: 45%
+
+`;
+
+const Character = (props) => {
+
+    const [data,setData] = useState([]);
+    const [url,setUrl] = useState('https://pokeapi.co/api/v2/pokemon/1');
+    const [pokemon, setPokemon] = useState('bulbasaur');
+    const [secondRender, setSecondRender] = useState(false);
+
+    useEffect(()=>{
+        axios.get(url)
+        .then(response=>{setData(response.data);setSecondRender(true)})
+    },[url])
+
+    const hanldeInputChange = (event) =>{
+        setPokemon(event.target.value.toLowerCase());
+    } 
+
+    const handleClick = (event) => {
+        props.list.map(element => {
+            if (element.name === pokemon){
+                setUrl(element.url);
+            }
+        });
+        return event;
+    }
+
+
+    return (
+        <div>
+            {secondRender ? 
+                    <div>
+                        <div>
+                            <Search type='text' placeholder='Pokemon Name' onChange={hanldeInputChange}/>
+                            <Button onClick={handleClick}> Search </Button>
+                        </div>
+                        <div>
+                            <Image src={data.sprites.other["official-artwork"].front_default}/>
+                        </div>
+                        <Container>
+                            <div>
+                                <p>Name: {data.species.name.toUpperCase()}</p>
+                                <p>Height: {data.height}dm</p>
+                                <p>- Base Stats -</p>
+                                <p>HP: {data.stats[0].base_stat}</p>
+                                <p>Attack: {data.stats[1].base_stat}</p>
+                                <p>Defense: {data.stats[2].base_stat}</p>
+                                <p>Special-attack: {data.stats[3].base_stat}</p>
+                                <p>Special-defense: {data.stats[4].base_stat}</p>
+                                <p>Speed: {data.stats[5].base_stat}</p>
+                            </div>
+
+                        </Container>
+                   </div>
+            : <p>Loading...</p>}
+        </div>
+
+    );
+}
+
+export default Character;
